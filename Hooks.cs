f43ee539e6f550082e32ca1a18d53f5c1f1f42e3
Skilotar <70480940+Skilotar@users.noreply.github.com>
2022-03-06 23:33:03 -0500

    Add files via upload

---
+++ Hooks.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+using MonoMod.RuntimeDetour;
+using UnityEngine;
+using Dungeonator;
+
+
+namespace AutoSemiAuto
+{
+    // Token: 0x02000018 RID: 24
+    public static class Hooks
+    {
+        // Token: 0x060000B7 RID: 183 RVA: 0x00008CE4 File Offset: 0x00006EE4
+        public static void Init()
+        {
+            try
+            {
+                Hook SetupUnlockTrackerComponent = new Hook(typeof(PlayerController).GetMethod("DoSpinfallSpawn", BindingFlags.Instance | BindingFlags.Public), typeof(Module).GetMethod("RunStartHook"));
+            }
+            catch (Exception e)
+            {
+                ItemAPI.Tools.PrintException(e, "FF0000");
+            }
+        }
+    }
+}
---
+++ PlayerAutoSemi.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using ItemAPI;
+using UnityEngine;
+using System.Collections;
+
+
+namespace AutoSemiAuto
+{
+	public class PlayerAutomaticSemiAutomatic : MonoBehaviour
+	{
+
+		public PlayerAutomaticSemiAutomatic()
+		{
+		
+
+		}
+
+		
+		public bool Key(GungeonActions.GungeonActionType action, PlayerController user)
+		{
+			return BraveInput.GetInstanceForPlayer(user.PlayerIDX).ActiveActions.GetActionFromType(action).IsPressed;
+		}
+
+		private void Start()
+		{
+			this.player = base.GetComponent<PlayerController>();
+
+		}
+
+
+		private void Update()
+		{
+			
+			Gun thisone = player.CurrentGun;
+			if (thisone.DefaultModule.shootStyle == ProjectileModule.ShootStyle.SemiAutomatic && Time.timeScale > 0f)
+			{
+				if (Key(GungeonActions.GungeonActionType.Shoot, player))
+				{
+					thisone.Attack();
+				}
+
+			}
+
+		}
+
+
+
+
+		public bool theatrefreebegotten;
+
+
+
+		private PlayerController player;
+	}
+}
